23bb5cdd12d375113cf95772bd2151a062ea8ffb
hmjag07 <harshjagtap153@gmail.com>
2025-01-04 16:38:39 +0530

    hashing password

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -14,4 +14,22 @@ const userSchema = new Schema({
     }
 })
 
+
+//static signup method
+userSchema.statics.signup = async function(email, password) {
+
+    const exists = await this.findOne({ email })
+
+    if(exists){
+        throw Error('Email already in use')
+}
+
+const salt = await bcrypt.genSalt(25) 
+const hash = await bcrypt.hash(password, salt)
+
+const user =  await this.create({ email, password: hash })
+
+return user
+}
+
 module.exports = mongoose.model('User', userSchema)
