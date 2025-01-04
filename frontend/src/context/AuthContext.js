ffac83ce102be766ab01ca3a5f0678fd54592573
hmjag07 <harshjagtap153@gmail.com>
2025-01-04 19:26:14 +0530

    error management

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -3,7 +3,7 @@ import { createContext, useReducer } from 'react'
 export const AuthContext = createContext()
 
 export const authReducer = (state, action) => {
-    switch (action, type){
+    switch (action.type){
         case 'LOGIN':
             return { user: action.payload }
         case 'LOGOUT':
