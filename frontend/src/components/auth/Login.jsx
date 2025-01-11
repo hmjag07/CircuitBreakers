2a226758b6045c27aca2feb6f22e5b43235cb8ec
Anvishaa <anvisha1309@gmail.com>
2025-01-11 23:46:10 +0530

    css changes of login ans signup

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 // import SignUp from './components/auth/Signup.jsx';
 import { Link } from 'react-router-dom'
-
+import './../../index.css'
 
 const Login = () => {
   useEffect(() => {
@@ -11,9 +11,11 @@ const Login = () => {
 
   return (
     <section className="bg-[#BED754] min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-extrabold text-center mb-10 -mt-16 text-[#3A3960]">
-        All At Home
+       <h1 className="text-6xl font-extrabold text-center mb-10 -mt-11 text-neutral paddin">Welcome Back !!
       </h1>
+      <h3 className="text-5xl font-extrabold text-center mb-10 -mt-1 text-neutral">
+        All At Home
+      </h3>
 
       <div className="w-full max-w-md bg-[#E8ECD7] rounded-lg shadow p-6 sm:p-8">
         <h2 className="text-2xl font-semibold text-center mb-6 text-[#3A3960]">
@@ -38,7 +40,7 @@ const Login = () => {
               name="password" 
               id="password" 
               placeholder="••••••••" 
-              className="bg-[#E8ECD7] border border-[#3A3960] text-[#3A3960] rounded-lg focus:ring-[#85A947] focus:border-[#85A947] block w-full p-2.5" 
+              className="custom-input" 
               required 
             />
           </div>
@@ -46,8 +48,9 @@ const Login = () => {
             <a href="#" className="text-sm font-medium text-[#85A947] hover:underline">Forgot password?</a>
           </div> */}
           <button 
+          
             type="submit" 
-            className="w-full text-[#3A3960] border-[#4e813a] bg-[#BED754] hover:bg-[#73ae35] focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className="custom-button"
           >
             Sign in
           </button>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,28 @@
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('flowbite/plugin'),
-    require('daisyui')
-  ],
+  plugins: [require('daisyui')],
   daisyui: {
-    themes: ["autumn"],
-  }
+    themes: [
+      {
+        mytheme: {
+          primary: "#BED754",
+          secondary: "#85A947",
+          accent: "#4e813a",
+          neutral: "#3A3960",
+          "base-100": "#E8ECD7",
+          "--rounded-box": "1rem",
+          "--rounded-btn": "0.5rem",
+          "--rounded-badge": "1.9rem",
+          "--animation-btn": "0.25s",
+          "--animation-input": "0.2s",
+          "--focus-ring":"0"
+        },
+      },
+    ],
+  },
 };
 
 
