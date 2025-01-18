b48c1aac66557d2834d0b446f4680ce4821bde10
ishika-demo <ishika2530@gmail.com>
2025-01-18 20:19:30 +0530

    proffesional

--- frontend/src/components/prof/Appoint.js
+++ frontend/src/components/prof/Appoint.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Layoutt from './Layoutt';
 const Appoint = () => {
   return (
+    <Layoutt>
     <div>
       <h1>APPOINTMENTS</h1>
     </div>
+    </Layoutt>
+
   );
 }
 
