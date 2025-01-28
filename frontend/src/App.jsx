47d7fb6d2ef7ba0b7606024f6b53018738b41b83
Anvishaa <anvisha1309@gmail.com>
2025-01-28 23:26:36 +0530

    error resolving

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 // import Login from './components/auth/Login.jsx' ;
 //  import Signin from './components/auth/Signin.jsx';
 // import SignUp from './components/auth/Signup.jsx';
-import {Routes, Route } from 'react-router-dom';
+// import {Routes, Route } from 'react-router-dom';
 import './index.css';
 
 
 import Appoint from './components/prof/Appoint.js';
 import About from './components/shared/About.js';
+import Request from './components/prof/Request.js'
 
 import ProfSignup from './components/auth/ProfSignup.jsx';
 import Missing from './components/shared/Missing.jsx';
-import Home from './components/shared/Home.jsx';
+// import Home from './components/shared/Home.jsx';
 import Editrequest from './components/prof/Editrequest.js';
 import Home from './components/shared/Home.jsx'
 
 import ProfCards from './components/resident/ProfCards.jsx';
 import Notice from './components/resident/Notice.jsx'
+import ResiHome from './components/resident/ProfCards.jsx'
 
 import { Routes, Route } from 'react-router-dom';
 import './index.css'
@@ -37,9 +39,8 @@ function App() {
         <Route path="/about" element={<About/>}/>
         {/* <Route path="/login" element={<Login/>}/> */}
         {/* <Route path="/resi/signup" element={<ResiSignup />} /> */}
-        <Route path="/user" element= {<ChooseUser/>}/>
         <Route path="/resi/home" element={<ResiHome/>}/>
-        <Route path="/prof/home" element={<ProfHome/>}/>
+        {/* <Route path="/prof/home" element={<ProfHome/>}/> */}
         <Route path="/prof/signup" element={<ProfSignup/>}/>
 
         <Route path="/request" element={<Request />} />
