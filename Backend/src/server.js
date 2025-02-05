e764dce15df9ad86823da22cbb006052b73759b8
hmjag07 <harshjagtap153@gmail.com>
2025-02-05 20:48:30 +0530

    changes in server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,25 +2,25 @@ const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const serviceRoutes = require('./routes/serviceRoutes');
+const requestRoutes = require('./routes/requestRoutes');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const { initializeServiceSocket } = require('./sockets/serviceSocket');
+const { initializeRequestSocket } = require('./sockets/requestSocket');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 const server = http.createServer(app);
-initializeServiceSocket(server);
+initializeRequestSocket(server);
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
-app.use('/api/services', serviceRoutes);
+app.use('/api/requests', requestRoutes);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
