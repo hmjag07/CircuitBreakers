56c96c7b231cef4d005594b3e36b117cb44e093d
Anvishaa <anvisha1309@gmail.com>
2025-02-06 13:26:12 +0530

    small chnage in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
-const requestRoutes = require('./src/routes/reqRoute');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const helmet = require('helmet');
@@ -12,14 +11,11 @@ const profRoute = require('./src/routes/profRoute')
 const requestRoutes = require('./src/routes/reqRoute.js')
 const app = express();
 const { initializeRequestSocket } = require('./src/sockets/requestSocket.js');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
 
 
 dotenv.config();
 connectDB(); 
 
-const { initializeRequestSocket } = require('./sockets/requestSocket');
 const authMiddleware = require('./src/middleware/authMiddleware');
 
 const server = http.createServer(app);
@@ -32,13 +28,14 @@ app.use(cors());
 app.use(helmet());
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, ()=>{
-    console.log('server running on : ${PORT}');
+    console.log(`server running on : ${PORT}`);
 });
 
 app.use('/api/requests', requestRoutes);
 app.use('/api', userRoute)
-const PORT = process.env.PORT || 3001;
 
 
 app.use('/data', profRoute);
