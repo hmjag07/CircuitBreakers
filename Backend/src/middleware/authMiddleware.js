0577ea1dbc1c0b8ffd1cff75ce5d02f77fbb3e3b
hmjag07 <harshjagtap153@gmail.com>
2025-02-06 16:32:43 +0530

    changes

--- Backend/src/middleware/authMiddleware.js
+++ Backend/src/middleware/authMiddleware.js
@@ -1,16 +1,28 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const User = require('../models/userModel');
 
-const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'Access Denied' });
+dotenv.config();
 
+
+// Middleware to authenticate JWT tokens
+ 
+const authMiddleware = async (req, res, next) => {
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
+        const token = req.header('Authorization');
+
+        if (!token) {
+            return res.status(401).json({ message: 'Access denied. No token provided.' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password'); // Exclude password from user object
+
         next();
     } catch (error) {
-        res.status(400).json({ message: 'Invalid Token' });
+        res.status(401).json({ message: 'Invalid or expired token' });
     }
 };
 
 module.exports = authMiddleware;
+
