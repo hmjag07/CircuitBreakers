7f0d68e42107ee482762e6861011d8f3c563b9ea
hmjag07 <harshjagtap153@gmail.com>
2025-02-11 14:33:11 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,44 +1,40 @@
 const express = require('express');
 const http = require('http');
 const dotenv = require('dotenv');
-const connectDB = require('./src/config/db');
+const connectDB = require('./config/db');
+const requestRoutes = require('./routes/reqRoute');
 const cors = require('cors');
-const bodyParser = require('body-parser')
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const userRoute = require('./src/routes/userRoute')
-const profRoute = require('./src/routes/profRoute')
-const requestRoutes = require('./src/routes/reqRoute.js')
-const app = express();
-const { initializeRequestSocket } = require('./src/sockets/requestSocket.js');
-
+const scheduleNoticeCleanup = require('./utils/scheduleNoticeDelete');
+const { initializeNoticeSocket } = require('./src/sockets/noticeSocket');
+const { initializeRequestSocket } = require('./src/sockets/requestSocket');
+const errorHandler = require('./middleware/errorHandler');
+const adminRoutes = require('./routes/adminRoute')
 
 dotenv.config();
-connectDB(); 
+connectDB();
 
-const authMiddleware = require('./src/middleware/authMiddleware');
-
+const app = express();
 const server = http.createServer(app);
 initializeRequestSocket(server);
+initializeNoticeSocket(server);
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, ()=>{
-    console.log(`server running on : ${PORT}`);
-});
+app.use('api/admin', adminRoutes);
 
 app.use('/api/requests', requestRoutes);
-app.use('/api', userRoute)
+const noticeRoutes = require('./routes/noticeRoute');
+app.use('/api/notices', noticeRoutes);
+
+// Start background job for auto-deleting notices
+scheduleNoticeCleanup();
 
 
-app.use('/data', profRoute);
 
-
-app.use('/api/requests', requestRoutes);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(` Server running on port ${PORT}`));
